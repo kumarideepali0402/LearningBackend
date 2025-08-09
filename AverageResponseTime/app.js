b365193a2f05d7afd109b7124c9e671d430d5631
Deepali <deepalikumari62004@gmail.com>
2025-08-10 00:25:37 +0530

    zod

--- AverageResponseTime/app.js
+++ AverageResponseTime/app.js
@@ -1,7 +1,21 @@
 const requestTime = require("./Middleware.js");
 const express = require("express");
 const app = express();
+const zod = require("zod")
 
+// const schema = zod.array(zod.number());
+// or
+const schema = zod.object({
+    email : zod.string().email(),
+    password : zod.string().min(8),
+    country: zod.literal("IN").or(zod.literal("US")),
+    kidneys: zod.array(zod.number())
+
+
+})
+
+
+// app.use(express()); Mandatory to be used while fetching the info from body
 app.use(requestTime);
 app.get('/', (req, res)=>{
     setTimeout(()=>{
@@ -9,4 +23,13 @@ app.get('/', (req, res)=>{
         
     }, 5000);
 })
+
+// global catches
+// error handling middleware
+app.use(function(err, req, res, next){
+    res.json({
+        msg:"sorry something is wrong with your input"
+    })
+})
+
 app.listen(3000);
